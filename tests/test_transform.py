8c4433d276b7d91fcf653e32b953ff9ddf236dec
David Dewhurst <ddewhurst@cra.com>
2021-11-11 09:54:10 -0500

    added another transform test

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -35,12 +35,32 @@ def mock_price_dataframe():
     ])
 
 
-def test_integration():
+def test_logdiff_stationary():
     records = mock_price_dataframe()
     records = records.resample('us').ffill()
     logging.info(f"Original records: {records}")
 
     s, s_inv = transform.make_stationarizer("logdiff")
+    stationary, centralized = transform.to_stationary(
+        np.mean, s, records
+    )
+    logging.info(f"Stationarized records: {stationary}")
+
+    nonstationary = s_inv(stationary, centralized)
+    logging.info(f"Nonstationary records: {nonstationary}")
+    assert pd.testing.assert_frame_equal(
+        nonstationary,
+        records.iloc[1:],
+        check_exact=False,
+    ) is None
+
+
+def test_diff_stationary():
+    records = mock_price_dataframe()
+    records = records.resample('us').ffill()
+    logging.info(f"Original records: {records}")
+
+    s, s_inv = transform.make_stationarizer("diff")
     stationary, centralized = transform.to_stationary(
         np.mean, s, records
     )
