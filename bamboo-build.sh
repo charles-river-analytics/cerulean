2f9cdc88c3005f95fcd09cfa4fbcbf2c4e80736d
Mike Reposa <mreposa@cra.com>
2021-12-03 16:17:44 -0500

    Bamboo build scripts

---
+++ bamboo-build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source /opt/anaconda/etc/profile.d/conda.sh
+
+rem Activate the conda environment
+conda activate proper-fm
+
+rem Update cerulean requirements
+python -m pip install -r requirements.txt
+
+rem Run unit tests and coverage
+python -m pytest --cov=cerulean
+
+rem Deactivate the environment
+conda deactivate
