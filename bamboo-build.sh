5c1986966cb717eb9e9789aeba95e65379353188
Mike Reposa <mreposa@cra.com>
2021-12-09 14:44:25 -0500

    Added conda init

--- bamboo-build.sh
+++ bamboo-build.sh
@@ -4,9 +4,9 @@ source /opt/conda/bin/conda
 
 conda init bash
 
-conda create --name proper-fm python=3.9
+conda create -y --name proper-fm python=3.9
 
-activate proper-fm
+activate -y proper-fm
 
 python -m pip install -r requirements.txt
 
