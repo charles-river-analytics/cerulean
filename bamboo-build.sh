4512398f1ed9ce83aee9e5eaefd56413f2f99a94
Mike Reposa <mreposa@cra.com>
2021-12-09 14:28:36 -0500

    Added conda init

--- bamboo-build.sh
+++ bamboo-build.sh
@@ -4,14 +4,10 @@ source /opt/conda/bin/conda
 
 conda init bash
 
-rem Activate the conda environment
-conda activate proper-fm
+activate proper-fm
 
-rem Update cerulean requirements
 python -m pip install -r requirements.txt
 
-rem Run unit tests and coverage
 python -m pytest --cov=cerulean
 
-rem Deactivate the environment
-conda deactivate
+deactivate
